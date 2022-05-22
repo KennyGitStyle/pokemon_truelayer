ea9bd9c08305a5939e0aeb9f39b48689edef2671
Kenneth Hendricks <kennethhendricks10@gmail.com>
2022-05-22 10:47:13 +0100

    first commit

--- Pokemon.Core/Pokemon.cs
+++ Pokemon.Core/Pokemon.cs
@@ -1,9 +1,5 @@
 namespace Pokemon.Core
 {
-    /*
-        Its good practice to create objects with an ID property for identification purposes. Epecially if you are going to be using a database.
-        In this case we will be using a database to store our pokemon, with this we will be storing our connection string in development settings as a form of abstraction. Then we will configure our pipeline to accept both our development and production database.
-    */
     public class Pokemon
     {
         public string? PokemonId { get; set; }
